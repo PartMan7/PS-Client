4ac1474d38fb574dbf1f2a67070454eaa328b15f
Parth Mane <parth.mane@sprinklr.com>
2024-10-31 02:37:45 +0530

    types: Fix AbilityFlags type

--- data.d.ts
+++ data.d.ts
@@ -4,16 +4,10 @@
 type Stats = 'atk' | 'def' | 'spa' | 'spd' | 'spe' | 'hp';
 type StatsTable = Record<Stats, number>;
 
-interface AbilityFlags {
-	breakable?: 1; // Can be suppressed by Mold Breaker and related effects
-	cantsuppress?: 1; // Ability can't be suppressed by e.g. Gastro Acid or Neutralizing Gas
-	failroleplay?: 1; // Role Play fails if target has this Ability
-	failskillswap?: 1; // Skill Swap fails if either the user or target has this Ability
-	noentrain?: 1; // Entrainment fails if user has this Ability
-	noreceiver?: 1; // Receiver and Power of Alchemy will not activate if an ally faints with this Ability
-	notrace?: 1; // Trace cannot copy this Ability
-	notransform?: 1; // Disables the Ability if the user is Transformed
-}
+type AbilityFlags = Partial<
+	Record<'breakable' | 'cantsuppress' | 'failroleplay' | 'failskillswap' | 'noentrain' | 'noreceiver' | 'notrace' | 'notransform', 1>
+>;
+
 
 export const abilities: Record<
 	string,
