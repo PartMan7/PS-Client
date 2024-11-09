000bb14044acbcc1f210df626b85ddd94c782cce
Parth Mane <parth.mane@sprinklr.com>
2024-11-09 20:33:24 +0530

    types: Fix Message typedefs to export class with type

--- classes/message.d.ts
+++ classes/message.d.ts
@@ -14,7 +14,7 @@ type MessageOpts = {
 	time: void | number;
 };
 
-type Message = {
+export default class Message {
 	author: User;
 	content: string;
 	raw: string;
@@ -23,44 +23,36 @@ type Message = {
 	isIntro: boolean;
 	awaited: boolean;
 	time: number;
-} & (
-	| {
-			type: 'chat';
-			target: Room;
-	  }
-	| {
-			type: 'pm';
-			target: User;
-	  }
-) & {
-		constructor(input: MessageOpts): void;
 
-		/**
-		 * Responds to the message
-		 * @param text - The text to respond (to the message) with
-		 * @returns A promise that resolves when the message is sent successfully
-		 */
-		reply(text: string): Promise<Message>;
+	type: 'chat' | 'pm';
+	target: this['type'] extends 'chat' ? Room : User;
 
-		/**
-		 * Privately responds to the message
-		 * @param text - The text to privately respond (to the message) with
-		 */
-		privateReply(text: string): void;
+	constructor(input: MessageOpts);
 
-		/**
-		 * Sends HTML in the message context (chatroom for 'chat', PMs for 'pm')
-		 * @param html - The HTML to send
-		 * @param opts - An instance of HTMLopts (name/rank/change)
-		 */
-		sendHTML(html: any, opts?: HTMLopts): boolean;
+	/**
+	 * Responds to the message
+	 * @param text - The text to respond (to the message) with
+	 * @returns A promise that resolves when the message is sent successfully
+	 */
+	reply(text: string): Promise<Message>;
 
-		/**
-		 * Privately sends HTML in the message context (chatroom for 'chat', PMs for 'pm')
-		 * @param html - The HTML to send
-		 * @param opts - An instance of HTMLopts (name/rank/change)
-		 */
-		replyHTML(html: any, opts?: HTMLopts): boolean;
-	};
+	/**
+	 * Privately responds to the message
+	 * @param text - The text to privately respond (to the message) with
+	 */
+	privateReply(text: string): void;
 
-export default Message;
+	/**
+	 * Sends HTML in the message context (chatroom for 'chat', PMs for 'pm')
+	 * @param html - The HTML to send
+	 * @param opts - An instance of HTMLopts (name/rank/change)
+	 */
+	sendHTML(html: any, opts?: HTMLopts): boolean;
+
+	/**
+	 * Privately sends HTML in the message context (chatroom for 'chat', PMs for 'pm')
+	 * @param html - The HTML to send
+	 * @param opts - An instance of HTMLopts (name/rank/change)
+	 */
+	replyHTML(html: any, opts?: HTMLopts): boolean;
+}
